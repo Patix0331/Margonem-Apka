b831c44cb0194ef96c9d9d9f47d7d9026db79ca4
Patix0331 <patix0331@gmail.com>
2019-06-24 18:45:00 +0200

    Added spaces between functions and clasess

--- Apka Bot/apka.py
+++ Apka Bot/apka.py
@@ -1,11 +1,13 @@
 class Characters:
+    
     name = []
     id = []
-
 class Apka:
+    
     import hashlib
     username = input("Podaj login: ").encode('ascii')
     password = input("Podaj hasło: ").encode('ascii')
+
     hash = hashlib.sha1
     hashed_password = hash(b"mleczko"+password).hexdigest()
 
@@ -15,32 +17,28 @@ class Apka:
             'l': username,
             'ph': ph
         })
-
         if("logged" in str(response.content)):
             return response
+
         else:
             raise Exception
-
     def chars(self):
+
         import re
         characters = re.findall(r"option label=\"(.*?)\" value=\"(.*?)\"", response.text)
         for char in characters:
+
             Character_List = Characters()
             Character_List.name.append(char[0])
             Character_List.id.append(char[1])
+
         return Character_List
 
         
 account = Apka()
 response = account.signIn()
-#<<<<<<< HEAD
-characters = account.chars()
-#just checking if i can edit
-#=======
 chars = account.chars()
-chars = list(zip(chars.name, chars.id))
 
 print(response.cookies, "\n")
+chars = list(zip(chars.name, chars.id))
 print(chars)
-#>>>>>>> 0186a6ee7b5e831803623e31a440dce798b8c421
-#stop updating now xd
